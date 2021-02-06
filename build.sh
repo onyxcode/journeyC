f12fc20635cd381145b8ed27fed02aab3cd253ef
onyxcode <me@onyxcode.net>
2021-02-06 00:08:53 -0500

    Removed Python compiler script in favor of much simpler Bash one

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3.8
+#!/bin/bash
 """
 
 ░█████╗░  ░█████╗░░█████╗░███╗░░░███╗██████╗░██╗██╗░░░░░███████╗██████╗░
@@ -12,34 +12,23 @@
 
 """
 
-import os
+while getopts f: flag
+do
+    case "${flag}" in
+        f) file=${OPTARG};;
+    esac
+done
 
-# Selects folder, eg day1
-folder = input("What day of the journey?\n> ")
-try:
-    # Make a variable with it
-    day = (f"day{folder}")
-# This was supposed to check if the directory existed, but it's broken
-except os.path.isdir(f"{day}") == False:
-    exit(1)
-else:
-    try:
-        # Make a variable with the exisiting C script
-        file = input("What's the file name?\n> ")
-        # Make a variable to define binary output
-        output = input("What's the output name?\n> ")
-        # Opens C script location to check if it exists
-        with open(f"{day}/{file}") as cs:
-            cs = cs
-    except FileNotFoundError:
-        print("This file/directory does not exist.")
-        exit(1)
-    else:
-        # Builds binary from C file
-        os.system(f"gcc {day}/{file} -Wall -Wextra -pedantic -o {day}/{output}")
-        # Makes binary executable
-        os.system(f"chmod +x {day}/{output}")
-        # Runs newly created binary
-        print("------------C Script Output------------\n")
-        os.system(f"./{day}/{output}")
-        print("\n--------------End of Output------------")
+if [[ -z "$file" ]]; then
+    echo "Must provide filename with -f" 1>&2
+    exit 1
+fi
+
+gcc ${file} -Wall -Wextra -pedantic -o ${file}.tmp
+{
+chmod +x ${file}.tmp
+} &> /dev/null
+./${file}.tmp
+{
+    rm ${file}.tmp
+} &> /dev/null
